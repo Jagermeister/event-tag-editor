95794111fbe5d5292bdf6da149e94f892220d107
Matthew Yeager <MatthewBrandonYeager+git@gmail.com>
2019-01-05 20:08:05 -0800

    Added basic Event Tag reporting

---
+++ src/app/event/event-tag-report/event-tag-report.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventService } from '../event.service';
+
+@Component({
+    selector: 'app-event-tag-report',
+    templateUrl: './event-tag-report.component.html',
+    styleUrls: ['./event-tag-report.component.css']
+})
+export class EventTagReportComponent implements OnInit {
+    tags = [];
+
+    constructor(private router: Router, private eventService: EventService) {
+        this.eventService.events.subscribe(events => {
+            const tagReduction = events.reduce((p, c) => {
+                c.tags.forEach(cTag => {
+                    const levels = cTag.split('-');
+                    const parent = levels.shift();
+                    const element = levels.join(' ');
+                    const parentLevel = p[parent] || Object.assign({}, {
+                        tag: '',
+                        count: 0,
+                        children: { }
+                    }, {tag: parent});
+                    parentLevel.count++;
+                    if (element !== '') {
+                        const elementCount = parentLevel.children[element] || {
+                            tag: element,
+                            count: 0
+                        };
+                        elementCount.count++
+                        parentLevel.children[element] = elementCount;
+                    }
+
+                    p[parent] = parentLevel;
+                });
+
+                return p;
+            }, {});
+
+            this.tags = Object.values(tagReduction).sort((a, b) => b['count'] - a['count']);
+            this.tags.forEach(t => t.children = Object.values(t.children).sort((a, b) => b['count'] - a['count']));
+        });
+    }
+
+    ngOnInit() {
+    }
+}
--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -5,6 +5,7 @@ import { EventGridComponent } from './event-grid/event-grid.component';
 import { EventFormComponent } from './event-form/event-form.component';
 import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EventTagReportComponent } from './event-tag-report/event-tag-report.component';
 
 const eventRoutes: Routes = [
     {
@@ -12,15 +13,20 @@ const eventRoutes: Routes = [
         component: EventGridComponent
     },
     {
+        path: 'report',
+        component: EventTagReportComponent
+    },
+    {
         path: ':id',
         component: EventFormComponent
-    },
+    }
 ];
 
 @NgModule({
     declarations: [
         EventFormComponent,
-        EventGridComponent
+        EventGridComponent,
+        EventTagReportComponent
     ],
     imports: [
         CommonModule,
